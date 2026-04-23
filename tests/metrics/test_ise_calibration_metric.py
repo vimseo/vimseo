1af3a5b2ddaad6e546a046a5fa14e49edc944aea
sebastien.bocquet <sebastien.bocquet@irt-saintexupery.com>
2026-04-23 17:50:56 +0200

    missing assertion in test

--- tests/metrics/test_ise_calibration_metric.py
+++ tests/metrics/test_ise_calibration_metric.py
@@ -21,15 +21,21 @@ import pytest
 from gemseo.datasets.dataset import Dataset
 from numpy import concatenate
 from numpy import linspace
+from numpy import ones
 
 from vimseo.tools.calibration.calibration_metrics import SBPISE
 from vimseo.utilities.curves_generator import expressions_convexity
 from vimseo.utilities.curves_generator import expressions_oscillate
 from vimseo.utilities.curves_generator import get_history
-from vimseo.utilities.curves_generator import n_points
 
 if TYPE_CHECKING:
     from numpy import ndarray
+
+
+N_POINTS = 10
+X_LEFT = -0.5
+X_RIGHT = 0.5
+DELTA_Y = 0.05
 
 
 def _swap_points(x: ndarray, y: ndarray):
@@ -41,6 +47,14 @@ def _swap_points(x: ndarray, y: ndarray):
 
 
 @pytest.mark.parametrize(
+    ("weight_left", "weight_right"),
+    [
+        (0.0, 0.0),
+        (1.0, 0.0),
+        (0.0, 1.0),
+    ],
+)
+@pytest.mark.parametrize(
     "is_decreasing_simulation",
     [False, True],
 )
@@ -51,7 +65,9 @@ def _swap_points(x: ndarray, y: ndarray):
 @pytest.mark.parametrize(
     ("x_left", "x_right"),
     [
-        (0.1, 0.9),
+        (X_LEFT, X_RIGHT),
+        (X_LEFT - 0.1, X_RIGHT + 0.1),
+        (X_LEFT + 0.1, X_RIGHT - 0.1),
     ],
 )
 @pytest.mark.parametrize(
@@ -62,33 +78,23 @@ def _swap_points(x: ndarray, y: ndarray):
     ],
 )
 def test_ise(
+    weight_left,
+    weight_right,
     x_left,
     x_right,
     is_decreasing_reference,
     is_decreasing_simulation,
     swap_points,
 ):
-    metric = SBPISE("y", "y_mesh", True, 1.0, 1.0)
+    metric = SBPISE("y", "y_mesh", True, weight_left, weight_right)
 
-    x_hist_exp = get_history(support=linspace(x_left, x_right, n_points))
-    y_hist_exp = get_history(
-        [
-            expressions_convexity["convex"],
-            expressions_oscillate["half_drop"],
-            "x*0.9",
-        ],
-        support=x_hist_exp,
-    )
+    # building a mock reference curve
+    x_hist_exp = linspace(x_left, x_right, N_POINTS)
+    y_hist_exp = ones(x_hist_exp.shape)
 
     # building a mock numerical curve
-    x_hist_num = get_history()
-    y_hist_num = get_history(
-        list_expressions=[
-            expressions_convexity["concave"],
-            expressions_oscillate["half_drop"],
-        ],
-        support=x_hist_num,
-    )
+    x_hist_num = linspace(X_LEFT, X_RIGHT, N_POINTS)
+    y_hist_num = ones(x_hist_num.shape) * (1.0 + DELTA_Y)
 
     if is_decreasing_reference:
         x_hist_exp = x_hist_exp[::-1]
@@ -105,22 +111,36 @@ def test_ise(
         data=[concatenate([x_hist_exp, y_hist_exp])],
         variable_names=["y_mesh", "y"],
         variable_names_to_n_components={
-            "y": n_points,
-            "y_mesh": n_points,
+            "y": N_POINTS,
+            "y_mesh": N_POINTS,
         },
     )
     model_dataset = Dataset.from_array(
         data=[concatenate([x_hist_num, y_hist_num])],
         variable_names=["y_mesh", "y"],
         variable_names_to_n_components={
-            "y": n_points,
-            "y_mesh": n_points,
+            "y": N_POINTS,
+            "y_mesh": N_POINTS,
         },
     )
 
     metric.set_reference_data(reference_data.to_dict_of_arrays(False))
     result = metric._evaluate_measure(model_dataset.to_dict_of_arrays(False))
-    print(result)
+
+    common_support = min(x_right - x_left, X_RIGHT - X_LEFT)
+    if weight_left == 0.0 and weight_right == 0.0:
+        assert result == pytest.approx(common_support * DELTA_Y**2)
+    elif weight_left == 0.0 and weight_right == 1.0:
+        assert result == pytest.approx(
+            0.5 * common_support * DELTA_Y**2 + 0.5 * (x_right - X_RIGHT) ** 2
+        )
+    elif weight_left == 1.0 and weight_right == 0.0:
+        assert result == pytest.approx(
+            0.5 * common_support * DELTA_Y**2 + 0.5 * (X_LEFT - x_left) ** 2
+        )
+    else:
+        msg = "Invalid weight combination"
+        raise ValueError(msg)
 
 
 @pytest.mark.parametrize(
@@ -133,36 +153,42 @@ def test_ise(
 def test_ise_disjoint_supports(x_left, x_right):
     metric = SBPISE("y", "y_mesh", True, 1.0, 1.0)
 
-    x_hist_exp = get_history(support=linspace(x_left, x_right, n_points))
-    y_hist_exp = get_history([
-        expressions_convexity["convex"],
-        expressions_oscillate["half_drop"],
-        "x*0.9",
-    ])
+    x_hist_exp = linspace(x_left, x_right, N_POINTS)
+    y_hist_exp = get_history(
+        [
+            expressions_convexity["convex"],
+            expressions_oscillate["half_drop"],
+            "x*0.9",
+        ],
+        nb_points=N_POINTS,
+        support=x_hist_exp,
+    )
 
     # building a mock numerical curve
-    x_hist_num = get_history()
+    x_hist_num = linspace(0.0, 1.0, N_POINTS)
     y_hist_num = get_history(
         list_expressions=[
             expressions_convexity["concave"],
             expressions_oscillate["half_drop"],
-        ]
+        ],
+        nb_points=N_POINTS,
+        support=x_hist_num,
     )
 
     reference_data = Dataset.from_array(
         data=[concatenate([x_hist_exp, y_hist_exp])],
         variable_names=["y_mesh", "y"],
         variable_names_to_n_components={
-            "y": n_points,
-            "y_mesh": n_points,
+            "y": N_POINTS,
+            "y_mesh": N_POINTS,
         },
     )
     model_dataset = Dataset.from_array(
         data=[concatenate([x_hist_num, y_hist_num])],
         variable_names=["y_mesh", "y"],
         variable_names_to_n_components={
-            "y": n_points,
-            "y_mesh": n_points,
+            "y": N_POINTS,
+            "y_mesh": N_POINTS,
         },
     )
 
