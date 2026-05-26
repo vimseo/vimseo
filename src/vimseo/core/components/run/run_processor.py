649a90bc92eeec7d19df529ed9104bf62cdec7e8
sebastien.bocquet <sebastien.bocquet@irt-saintexupery.com>
2026-05-26 14:32:07 +0200

    clean up useless logic in external software subprocess, and ensure the external software is killed in any case

--- src/vimseo/core/components/run/run_processor.py
+++ src/vimseo/core/components/run/run_processor.py
@@ -1,3 +1,18 @@
+# Copyright 2021 IRT Saint Exupery, https://www.irt-saintexupery.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
 #
 # This program is free software; you can redistribute it and/or
@@ -18,7 +33,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from vimseo.core.components.external_software_component import ExternalSoftwareComponent
-from vimseo.job_executor.base_executor import BaseJobExecutor
+from vimseo.job_executor.base_executor import JobExecutor
 from vimseo.job_executor.base_user_job_options import BaseUserJobSettings
 
 if TYPE_CHECKING:
@@ -41,7 +56,7 @@ class RunProcessor(ExternalSoftwareComponent):
         """
         super().__init__(**options)
         self.subroutine_list = []
-        self._job_executor = BaseJobExecutor("")
+        self._job_executor = JobExecutor("")
         self._user_job_options = BaseUserJobSettings()
 
     @property
