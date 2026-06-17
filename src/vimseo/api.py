9f990f5b8fd7eb88b6545dcb88f9c622ab0274a9
sebastien.bocquet <sebastien.bocquet@irt-saintexupery.com>
2026-06-17 10:28:57 +0200

    try fix config extension

--- src/vimseo/api.py
+++ src/vimseo/api.py
@@ -1,3 +1,18 @@
+# Copyright 2021 IRT Saint Exupery, https://www.irt-saintexupery.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
 #
 # This program is free software; you can redistribute it and/or
@@ -29,7 +44,7 @@ from typing import TYPE_CHECKING
 from gemseo import configure_logger
 from gemseo.utils.metrics.metric_factory import MetricFactory
 
-from vimseo.config.global_configuration import _configuration as configuration
+# from vimseo.config.global_configuration import _configuration as configuration
 from vimseo.core.load_case_factory import LoadCaseFactory
 from vimseo.core.model_factory import ModelFactory
 from vimseo.core.model_settings import IntegratedModelSettings
@@ -44,7 +59,7 @@ LOGGER = logging.getLogger(__name__)
 
 def activate_logger(level: int | None = None):
     if not level:
-        level = _nameToLevel[configuration.logging.upper()]
+        level = _nameToLevel["INFO"]
     configure_logger(level=level)
 
 
@@ -150,14 +165,14 @@ def get_available_tools():
     return class_names
 
 
-def print_config():
-    """Returns: representation of the current configuration variables."""
-    LOGGER.info(configuration.model_dump())
+# def print_config():
+#     """Returns: representation of the current configuration variables."""
+#     LOGGER.info(configuration.model_dump())
 
 
-def print_config_help():
-    """Return the help about configuration file."""
-    LOGGER.info(configuration.model_fields)
+# def print_config_help():
+#     """Return the help about configuration file."""
+#     LOGGER.info(configuration.model_fields)
 
 
-activate_logger()
+# activate_logger()
