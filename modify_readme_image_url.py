3cfbce58f9a8166dc72d2eab276d3499d06af07c
Sebastien Bocquet <sebastien.bocquet@irt-saintexupery.com>
2026-06-17 21:51:54 +0200

    Deployed 0f82a7ac with MkDocs version: 1.6.1

--- modify_readme_image_url.py
+++ modify_readme_image_url.py
@@ -5,4 +5,8 @@ import pathlib
 if __name__ == "__main__":
     content = pathlib.Path("README.md").read_text()
     content = content.replace("/docs/images", "images")
+    content = content.replace(
+        "[Contributing](https://vimseo.github.io/vimseo/contributing/)",
+        "[Contributing](contributing.md)",
+    )
     pathlib.Path("README_tmp.md").write_text(content)
